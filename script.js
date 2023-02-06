82e2534f5ffb86afe4a572356ba8c4d8a723d496
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-02-06 16:43:55 +0000

    added css folder and file as well as for loop to add button in html with attributes and class

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 
 const weatherAPIURL = "https://api.openweathermap.org";
 const weatherAPIKey = "3e57f9528237c8e378dd6f85ab408b45";
-let searchInput = $("#search-input");
+let searchHistory = []
 
+let searchInput = $("#search-input");
 let searchForm = $("#search-form");
+let searchHisContainer = $("#history")
 
 function fetchCoord(search){
     let queryURL = `${weatherAPIURL}/geo/1.0/direct?q=${search}&limit=5&appid=${weatherAPIKey}`
     console.log(queryURL);
 
 
-    fetch(queryURL).then(function(data){
+    fetch(queryURL, {method: "GET"}).then(function(data){
         return data.json()
     }).then(function(response){
-        console.log(response);
+        if(!response[0]){
+            alert("Location not found")
+        } else {
+
+            if(searchHistory.indexOf(search) !== -1){
+                return
+            }
+            searchHistory.push(search);
+
+            localStorage.setItem("search-history", JSON.stringify(searchHistory));
+
+            searchHisContainer.html("")
+
+            for(let i = 0; i < searchHistory.length; i++){
+                let btn = $("<button>");
+                btn.attr("type", "button")
+                btn.addClass("history-btn btn-history")
+
+                btn.attr("data-search", searchHistory[i])
+                searchHisContainer.append(btn)
+            }
+        };
     })
 }
 
