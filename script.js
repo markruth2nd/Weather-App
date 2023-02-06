8d03a671175a48f586b974335a3f562a09c9a997
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-02-06 17:04:46 +0000

    added functions in JS to render a search history, append the search history as well as initialize the search history from the local storage

--- script.js
+++ script.js
@@ -7,6 +7,31 @@ let searchInput = $("#search-input");
 let searchForm = $("#search-form");
 let searchHisContainer = $("#history")
 
+
+function renderSearchHistory(){
+    searchHisContainer.html("")
+        for(let i = 0; i < searchHistory.length; i++){
+        let btn = $("<button>");
+        btn.attr("type", "button")
+        btn.addClass("history-btn btn-history")
+
+        btn.attr("data-search", searchHistory[i])
+        btn.text(searchHistory[i])
+        searchHisContainer.append(btn)           
+    }
+}
+
+function appendSearchHistory(search){
+    if(searchHistory.indexOf(search) !== -1){
+        return
+    }
+    searchHistory.push(search);
+
+    localStorage.setItem("search-history", JSON.stringify(searchHistory));
+
+    renderSearchHistory()
+}
+
 function fetchCoord(search){
     let queryURL = `${weatherAPIURL}/geo/1.0/direct?q=${search}&limit=5&appid=${weatherAPIKey}`
     console.log(queryURL);
@@ -18,26 +43,18 @@ function fetchCoord(search){
         if(!response[0]){
             alert("Location not found")
         } else {
-
-            if(searchHistory.indexOf(search) !== -1){
-                return
+            appendSearchHistory(search)
             }
-            searchHistory.push(search);
-
-            localStorage.setItem("search-history", JSON.stringify(searchHistory));
-
-            searchHisContainer.html("")
-
-            for(let i = 0; i < searchHistory.length; i++){
-                let btn = $("<button>");
-                btn.attr("type", "button")
-                btn.addClass("history-btn btn-history")
-
-                btn.attr("data-search", searchHistory[i])
-                searchHisContainer.append(btn)
-            }
-        };
     })
+}
+
+function initialzeHistory(){
+    let storedHistory = localStorage.getItem("search-history");
+
+    if(storedHistory) {
+        searchHistory = JSON.parse(storedHistory);
+    }
+    renderSearchHistory()
 }
 
 function submitSearch(event){
@@ -47,5 +64,7 @@ function submitSearch(event){
 
     fetchCoord(search);
 }
+
+initialzeHistory();
 
 searchForm.on("submit", submitSearch);
